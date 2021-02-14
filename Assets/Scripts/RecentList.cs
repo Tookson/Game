dcf16111de558f09bc959543c229d258c3d165d7
IshimichiReo <asozinov03@gmail.com>
2021-02-15 02:43:50 +0300

    Жесть

--- Assets/Scripts/RecentList.cs
+++ Assets/Scripts/RecentList.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 
 public class RecentList : MonoBehaviour
 {
-    public Color color1 = Color.white;
+    public Color color1 = Color.gray;
     public Color color2 = Color.green;
-    public Color color3 = Color.red;
-    public Color color4 = Color.blue;
-    public Color color5 = Color.black;
-    public virtual int Count { get; }
+    public Color color3 = Color.blue;
     public List<string> recentList;
+    public virtual int Count { get; }
+    public GameObject Sphere;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -27,9 +26,9 @@ public class RecentList : MonoBehaviour
             renderer.material.color = color1;
         }
 
-        if (Count == 10)
+        if (recentList.Count == 10) 
         {
-            renderer.material.color = color4;
+            Sphere.GetComponent<SpriteRenderer>().color = new Color(100, 100, 100, 100);
         }
 
         if (isVisited)
